e3f7b63777aee93a46f5c4805a79f12f67d0388b
Dinesh Babu <56795144+holdmycaramelpopcorn@users.noreply.github.com>
2020-06-10 17:26:07 +0530

    Add files via upload

---
+++ Objects/weighingmain.d
@@ -0,0 +1,5 @@
+.\objects\weighingmain.o: weighingmain.c
+.\objects\weighingmain.o: C:\Keil_v5\ARM\INC\Philips\lpc214x.h
+.\objects\weighingmain.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\objects\weighingmain.o: LCD.h
+.\objects\weighingmain.o: matrix.h
